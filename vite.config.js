2064d2ada2182c0ba444462ecd2fa6a1e73b8ba1
Baga Hizriev <bagahizriev@yandex.ru>
2026-03-15 08:54:43 +0400

    -

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,8 @@ export default defineConfig({
                 clientsClaim: true,
                 cleanupOutdatedCaches: true,
                 globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+                navigateFallback: "index.html",
+                navigateFallbackDenylist: [/^\/_/, /\/[^/?]+\.[^/]+$/],
                 runtimeCaching: [
                     {
                         urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
@@ -37,15 +39,16 @@ export default defineConfig({
                 theme_color: "#2563eb",
                 background_color: "#ffffff",
                 display: "standalone",
-                start_url: "/",
+                start_url: "./",
+                scope: "./",
                 icons: [
                     {
-                        src: "/icon-192.png",
+                        src: "./icon-192.png",
                         sizes: "192x192",
                         type: "image/png",
                     },
                     {
-                        src: "/icon-512.png",
+                        src: "./icon-512.png",
                         sizes: "512x512",
                         type: "image/png",
                     },
